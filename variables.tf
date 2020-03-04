f477337dd17cefee1c49d0f76e7fd07ef4b02339
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-03-04 15:40:56 -0500

    updated variables

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "subnet_prefix" {
 
 variable "vm_size" {
   description = "Specifies the size of the virtual machine."
-  default     = "Standard_A1"
+  default     = "Standard_A0"
 }
 
 variable "image_publisher" {
