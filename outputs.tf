331daecf391520e104b186cd2ff0111bb1773cbc
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-03-04 17:52:33 -0500

    output doesn't matter

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 // Comment out the below stanza to show the modules section
 
 # Outputs file
-// output "catapp_url" {
-//   value = "http://${azurerm_public_ip.catapp-pip.fqdn}"
-// }
+output "catapp_url" {
+  value = "http://${azurerm_public_ip.catapp-pip.fqdn}"
+}
 
 output "container_app_url" {
    value = "http://${module.web_app_container.hostname}"
