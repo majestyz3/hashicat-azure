f4e5e81a382aaeecfa2790b14d27e4c10aa9a534
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-03-04 15:37:21 -0500

    check modules

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
 # Outputs file
-output "catapp_url" {
-  value = "http://${azurerm_public_ip.catapp-pip.fqdn}"
+// output "catapp_url" {
+//   value = "http://${azurerm_public_ip.catapp-pip.fqdn}"
+// }
+
+output "container_app_url" {
+   value = "http://${module.web_app_container.hostname}"
 }
